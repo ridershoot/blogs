ab5b948bf03919ae65ccd3c8c1123ddbf5a54219
lixiaofeng <1903655014@qq.com>
2025-02-28 15:08:52 +0800

    feat: 修改github图标点击跳转地址为github page所在页面

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -23,6 +23,6 @@ export default defineConfig({
 			},
 		],
 
-		socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+		socialLinks: [{ icon: "github", link: "https://ridershoot.github.io/blogs" }],
 	},
 });
