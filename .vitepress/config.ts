21cfb230afd38dbe093a8947a75ce70133265feb
lixiaofeng <1903655014@qq.com>
2025-02-28 15:10:31 +0800

    feat: 修改github图标点击跳转地址为github仓库所在页面

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -23,6 +23,6 @@ export default defineConfig({
 			},
 		],
 
-		socialLinks: [{ icon: "github", link: "https://ridershoot.github.io/blogs" }],
+		socialLinks: [{ icon: "github", link: "https://github.com/ridershoot/blogs" }],
 	},
 });
