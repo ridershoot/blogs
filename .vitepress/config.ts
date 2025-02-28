12a0b3848568cbc82d527eae7d2ddfe69e5390e8
lixiaofeng <1903655014@qq.com>
2025-02-28 15:01:09 +0800

    ci: 配置github actions&调整打包配置

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Programmer Li's Blog",
-  description: "Programmer Li's blog site, about front-end development, web performance, and so on.",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+	title: "Programmer Li's Blog",
+	base: "/blogs/",
+	description:
+		"Programmer Li's blog site, about front-end development, web performance, and so on.",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Examples", link: "/markdown-examples" },
+		],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+		sidebar: [
+			{
+				text: "Examples",
+				items: [
+					{ text: "Markdown Examples", link: "/markdown-examples" },
+					{ text: "Runtime API Examples", link: "/api-examples" },
+				],
+			},
+		],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+		socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+	},
+});
